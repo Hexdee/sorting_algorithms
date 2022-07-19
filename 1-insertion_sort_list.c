ff48ffb06713bbae3a7b2c9aec776d8302fbcc78
hexdee <lukmansaheed79@gmail.com>
2022-07-19 22:49:45 +0000

    Create bubble sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,17 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm
+ * @list: the list to be sorted
+*/
+
+void insertion_sort_list(listint_t **list)
+{
+	int num;
+
+	while(list->next)
+	{
+
+	}
+}
