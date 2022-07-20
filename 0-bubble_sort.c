8426b3cf3c8aec3625ed37344d2312e42c16def2
hexdee <lukmansaheed79@gmail.com>
2022-07-20 15:26:51 +0000

    Correct text answer order

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,7 +4,7 @@
  * bubble_sort - sorts an array of integers in ascending order
  * using the Bubble sort algorithm
  * @array: the array of integer to be sorted
- * @ize: size of the array
+ * @size: size of the array
 */
 
 void bubble_sort(int *array, size_t size)
